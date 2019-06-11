431851fce6eacda80c33064564d18f7d1608c457
Eduardo <eduardoluissd@gmail.com>
2019-06-11 09:00:15 -0500

    Finishing first chart correctly with random colors and average line.

--- public/src/insights.js
+++ public/src/insights.js
@@ -29,13 +29,15 @@ function getBBVAServiceReport(){
                 data: {
                   labels: count[0],
                   datasets: [{
+                    label: 'Promedio',
+                    data: getAverageList(count[1]),
+                    fill: false,
+                    borderColor: 'red',
+                    type: 'line'
+                  },{
                     label: 'Registros en BDs',
                     data: count[1],
-                    backgroundColor: [
-                        'rgba(255, 186, 36, 0.7)',
-                        'rgba(10, 100, 235, 0.7)',
-                        'rgba(255, 10, 10, 0.7)'
-                    ]
+                    backgroundColor: getRandomColor(count[1].length)
                   }]
                 },
                 options : {
@@ -48,8 +50,9 @@ function getBBVAServiceReport(){
                                 beginAtZero: true   // minimum value will be 0.
                             }
                         }]
-                    }
-                }
+                    },
+                },
+                
               });
         });
     });
@@ -59,18 +62,53 @@ function getBBVAServiceReport(){
 
 function countByDate(documents){
     return new Promise(resolve => {
-        let dates  ={
-            '25-01-2019' : 23,
-            '26-01-2019' : 33,
-            '27-01-2019' : 28,
-            '28-01-2019' : 23,
-            '29-01-2019' : 33,
-            '30-01-2019' : 28,
-            '31-01-2019' : 23,
-            '32-01-2019' : 33,
-            '33-01-2019' : 28,
-        };
+        
+        let dates  ={};
+        
+        for(let i = 0; i < documents.length; i++){
+            console.log(documents[i])
+            start_date = documents[i].doc['fecha_inicio'].substring(0,10);
+            if(dates[start_date]){
+                dates[start_date] += 1;
+            }
+            else{
+                dates[start_date] = 1;
+            }
+        }
+        dates = orderObject(dates)
         console.log(Object.keys(dates), Object.values(dates))
         resolve([Object.keys(dates), Object.values(dates)])
     });
+}
+
+function orderObject(unordered){
+
+    const ordered = {};
+    Object.keys(unordered).sort().forEach(function(key) {
+        ordered[key] = unordered[key];
+    });
+    return ordered
+}
+
+function getRandomColor(n) {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    var colors = [];
+    for(var j = 0; j < n; j++){
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        colors.push(color);
+        console.log(colors);
+        color = '#';
+    }
+    return colors;
+}
+
+/* Return a list with the average of the input */
+
+function getAverageList(data_list){
+    let sum = data_list.reduce((previous, current) => current += previous);
+    let avg = sum / data_list.length;
+    return Array(data_list.length).fill(avg)
 }
