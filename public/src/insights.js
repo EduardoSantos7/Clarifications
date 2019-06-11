899aea2a14f7acfa7a6df89bc8934417aa607ac4
Eduardo <eduardoluissd@gmail.com>
2019-06-11 10:08:43 -0500

    insights with semaphores (with clarifications and not)

--- public/src/insights.js
+++ public/src/insights.js
@@ -15,6 +15,10 @@ const Swal = require('sweetalert2')
 function loadCharts(){
     
     getBBVAServiceReport();
+
+    getSemaphoreClarifications();
+
+    getSemaphoreWithoutClarifications();
 }
 
 function getBBVAServiceReport(){
@@ -33,7 +37,7 @@ function getBBVAServiceReport(){
                     data: getAverageList(count[1]),
                     fill: false,
                     borderColor: 'red',
-                    type: 'line'
+                    type: 'line',
                   },{
                     label: 'Registros en BDs',
                     data: count[1],
@@ -43,6 +47,119 @@ function getBBVAServiceReport(){
                 options : {
                     scales: {
                         yAxes: [{
+                            display: true,
+                            ticks: {
+                                suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                                // OR //
+                                beginAtZero: true   // minimum value will be 0.
+                            }
+                        }]
+                    },
+                },
+                
+              });
+        });
+    });
+}
+
+function getSemaphoreClarifications(){
+
+    getAllRecords(process.env.REJOINDER_DB).then( (documents) => {
+        countSemaphoresByDate(documents).then((result) => {
+            var popCanvas = document.getElementById("canvas3");
+            var popCanvas = document.getElementById("canvas3").getContext("2d");
+
+            var barChart = new Chart(popCanvas, {
+                type: 'bar',
+                data: {
+                  labels: result['labels'],
+                  datasets: [
+                    {
+                      label: 'No cuenta',
+                      data: result['not_count'],
+                      backgroundColor: 'lightgrey'
+                    },
+                    {
+                      label: 'Rojo',
+                      data: result['red'],
+                      backgroundColor: 'rgba(255, 10, 10, 0.7)'
+                    },
+                    {
+                      label: 'Amarillo',
+                      data: result['yellow'],
+                      backgroundColor: 'rgba(255, 186, 36, 0.7)'
+                    },
+                    {
+                        label: 'Verde',
+                        data: result['green'],
+                        backgroundColor: 'lightgreen'
+                    },
+                    {
+                        label: 'Abierto',
+                        data: result['open'],
+                        backgroundColor: 'pink'
+                    }
+                  ]
+                },
+                options : {
+                    scales: {
+                        xAxes: [{ stacked: true }],
+                        yAxes: [{
+                            stacked: true,
+                            display: true,
+                            ticks: {
+                                suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                                // OR //
+                                beginAtZero: true   // minimum value will be 0.
+                            }
+                        }]
+                    },
+                },
+                
+              });
+        });
+    });
+}
+
+function getSemaphoreWithoutClarifications(){
+    
+    getAllRecords(process.env.CLARIFICATION_DB).then( (documents) => {
+        countSemaphoresByDate(documents).then((result) => {
+            var popCanvas = document.getElementById("canvas4");
+            var popCanvas = document.getElementById("canvas4").getContext("2d");
+
+            var barChart = new Chart(popCanvas, {
+                type: 'bar',
+                data: {
+                  labels: result['labels'],
+                  datasets: [
+                    {
+                      label: 'No cuenta',
+                      data: result['not_count'],
+                      backgroundColor: 'lightgrey'
+                    },
+                    {
+                      label: 'Rojo',
+                      data: result['red'],
+                      backgroundColor: 'rgba(255, 10, 10, 0.7)'
+                    },
+                    {
+                      label: 'Amarillo',
+                      data: result['yellow'],
+                      backgroundColor: 'rgba(255, 186, 36, 0.7)'
+                    },
+                    {
+                        label: 'Verde',
+                        data: result['green'],
+                        backgroundColor: 'lightgreen'
+                      }
+                  ]
+                },
+                options : {
+                    scales: {
+                        xAxes: [{ stacked: true }],
+                        yAxes: [{
+                            stacked: true,
                             display: true,
                             ticks: {
                                 suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
@@ -78,6 +195,83 @@ function countByDate(documents){
         dates = orderObject(dates)
         console.log(Object.keys(dates), Object.values(dates))
         resolve([Object.keys(dates), Object.values(dates)])
+    });
+}
+
+/* Returns an object with dates as key and array for each semaphore with correspond value per day
+    [no cuenta, rojo, amarillo, verde, abierto]
+*/
+
+function countSemaphoresByDate(documents){
+    return new Promise(resolve => {
+        
+        let dates  ={};
+        let not_count = [];
+        let red = [];
+        let yellow = [];
+        let green = [];
+        let open = [];
+        
+        for(let i = 0; i < documents.length; i++){
+            start_date = documents[i].doc['fecha_inicio'].substring(0,10);
+            if(dates[start_date]){
+
+                let semaphore = documents[i].doc['semaforo'].toLowerCase();
+                if(semaphore.includes('no cuenta')){
+                    dates[start_date][0]++;
+                }
+                else if(semaphore.includes('rojo')){
+                    dates[start_date][1]++;
+                }
+                else if(semaphore.includes('amarillo')){
+                    dates[start_date][2]++;
+                }
+                else if(semaphore.includes('verde')){
+                    dates[start_date][3]++;
+                }
+                else if(semaphore.includes('abierto')){
+                    dates[start_date][4]++;
+                }
+            }
+            else{
+                dates[start_date] = [0,0,0,0,0];
+
+                let semaphore = documents[i].doc['semaforo'].toLowerCase();
+                if(semaphore.includes('no cuenta')){
+                    dates[start_date][0]++;
+                }
+                else if(semaphore.includes('rojo')){
+                    dates[start_date][1]++;
+                }
+                else if(semaphore.includes('amarillo')){
+                    dates[start_date][2]++;
+                }
+                else if(semaphore.includes('verde')){
+                    dates[start_date][3]++;
+                }
+                else if(semaphore.includes('abierto')){
+                    dates[start_date][4]++;
+                }
+            }
+        }
+        dates = orderObject(dates);
+        let keys = Object.keys(dates);
+        for(let i = 0; i < keys.length; i++){
+            not_count.push(dates[keys[i]][0]);
+            red.push(dates[keys[i]][1]);
+            yellow.push(dates[keys[i]][2]);
+            green.push(dates[keys[i]][3]);
+            open.push(dates[keys[i]][4]);
+        }
+        let result = {
+            'labels': Object.keys(dates),
+            'not_count': not_count,
+            'red': red,
+            'yellow': yellow,
+            'green': green,
+            'open': open
+        }
+        resolve(result)
     });
 }
 
